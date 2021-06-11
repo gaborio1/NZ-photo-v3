ee26a83ce6487e1e3b4fe5721e665505eab5a4a4
gaborio1 <welcome2thepleasuredome@googlemail.com>
2021-06-11 21:07:05 +0100

    add navbar.js/css to lightbox2:, no opacity change on scroll yet

--- js-lightbox2:1.js
+++ js-lightbox2:1.js
@@ -1,60 +1,60 @@
-// ========== STICKY NAVBAR W3SCHOOLS ==========
+// // ========== STICKY NAVBAR W3SCHOOLS ==========
 
-window.onscroll = function() {myFunction()};
-const navbar = document.getElementById("header");
-// Get the offset position of the navbar
-const sticky = navbar.offsetTop;
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+// window.onscroll = function() {myFunction()};
+// const navbar = document.getElementById("header");
+// // Get the offset position of the navbar
+// const sticky = navbar.offsetTop;
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
 
-// ========== SEARCH BAR ==========
+// // ========== SEARCH BAR ==========
 
-const defaultButton = document.getElementById("default-btn");
-const submitButton = document.getElementById("submit-btn");
-const closeButton = document.getElementById("close-btn");
-const searchInput = document.getElementById("search-input");
+// const defaultButton = document.getElementById("default-btn");
+// const submitButton = document.getElementById("submit-btn");
+// const closeButton = document.getElementById("close-btn");
+// const searchInput = document.getElementById("search-input");
 
-const imageContainer = document.getElementById("image-container");
+// const imageContainer = document.getElementById("image-container");
 
-defaultButton.addEventListener("click", function() {
-    defaultButton.classList.add("hidden");
-    submitButton.classList.remove("hidden");
-    searchInput.classList.remove("hidden");
-    // searchInput.focus();
-    closeButton.classList.remove("hidden");
-    imageContainer.classList.add("blurred");
+// defaultButton.addEventListener("click", function() {
+//     defaultButton.classList.add("hidden");
+//     submitButton.classList.remove("hidden");
+//     searchInput.classList.remove("hidden");
+//     // searchInput.focus();
+//     closeButton.classList.remove("hidden");
+//     imageContainer.classList.add("blurred");
 
-    searchInput.style.width = "140px";
-})
+//     searchInput.style.width = "140px";
+// })
 
-closeButton.addEventListener("click", function() {
-    defaultButton.classList.remove("hidden");
-    submitButton.classList.add("hidden");
-    searchInput.classList.add("hidden");
-    closeButton.classList.add("hidden");
-    imageContainer.classList.remove("blurred");
+// closeButton.addEventListener("click", function() {
+//     defaultButton.classList.remove("hidden");
+//     submitButton.classList.add("hidden");
+//     searchInput.classList.add("hidden");
+//     closeButton.classList.add("hidden");
+//     imageContainer.classList.remove("blurred");
     
-    searchInput.style.width = "0px";
-})
+//     searchInput.style.width = "0px";
+// })
 
-// ========== ACTIVE NAV-LINK ==========
+// // ========== ACTIVE NAV-LINK ==========
 
-// Add active class to the current button (highlight it)
-// var header = document.getElementById("myDIV");
-const btns = document.getElementsByClassName("select-able");
-for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  const current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
-}
+// // Add active class to the current button (highlight it)
+// // var header = document.getElementById("myDIV");
+// const btns = document.getElementsByClassName("select-able");
+// for (let i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//   const current = document.getElementsByClassName("active");
+//   current[0].className = current[0].className.replace(" active", "");
+//   this.className += " active";
+//   });
+// }
 
 
 // ==================== DYNAMIC MODAL BASED ON W3SCHOOLS: https://www.w3schools.com/howto/howto_js_lightbox.asp ====================
