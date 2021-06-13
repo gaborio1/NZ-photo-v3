b91f6e332097bf0b86e66bcbaa4a67ca452c77e0
gaborio1 <welcome2thepleasuredome@googlemail.com>
2021-06-13 15:17:58 +0100

    refactor navbar.js for common navbar - MacBook

--- navbar.js
+++ navbar.js
@@ -1,3 +1,64 @@
+document.write('\
+\
+<div class="header-content"><div class="logo-container"><div class="logo__block-row"><div class="logo__block logo__block-1"></div><div class="logo__block logo__block-2"></div><div class="logo__block logo__block-3"></div></div></div><nav><a class="nav-link select-able active" href="#">Home</a><a class="nav-link select-able" href="pages/about.html">About</a><div class="dropdown"><button class="dropbtn select-able">portfolio <!-- <i class="fa fa-caret-down"></i> --></button><div class="dropdown-content"><a href="pages/model.html">model</a><a href="pages/portrait.html">portrait</a><a href="pages/nature.html">nature</a><a href="#">! street !</a><a href="#">! motion !</a><a href="#">! abstract !</a><a href="pages/js-lightbox-test.html">lightbox js 3:2</a><a href="pages/lightbox2:1.html">lightbox js 2:1</a></div></div> <div class="dropdown"><button class="dropbtn select-able">products <!-- <i class="fa fa-caret-down"></i> --></button><div class="dropdown-content"><a href="#">Link 1</a><a href="#">Link 2</a><a href="#">Link 3</a><a href="#">Link 4</a></div></div> <a class="nav-link select-able" href="pages/contact.html">Contact</a></nav><div class="search-container"><form action=""><button type="submit" id="submit-btn" class="submit-btn hidden"><i class="fa fa-search"></i></button><input type="text" placeholder="Search enzee.com" name="search" id="search-input" class="hidden" required><button type="" id="default-btn" class="default-btn"><i class="fa fa-search"></i></button><button type="" id="close-btn" class="close-btn hidden">X<!-- <i class="fa fa-times"></i> --></button></form></div></div>\
+\
+');
+
+{/* <div class="header-content">
+                        <div class="logo-container">
+                            <div class="logo__block-row">
+                                <div class="logo__block logo__block-1"></div>
+                                <div class="logo__block logo__block-2"></div>
+                                <div class="logo__block logo__block-3"></div>
+                            </div>
+                        </div>
+                        <nav>
+                            <a class="nav-link select-able active" href="../index.html">Home</a>
+                            <a class="nav-link select-able" href="about.html">About</a>
+                            <div class="dropdown">
+                                <button class="dropbtn select-able">portfolio 
+                                    <!-- <i class="fa fa-caret-down"></i> -->
+                                </button>
+                                <div class="dropdown-content">
+                                    <a href="model.html">model</a>
+                                    <a href="portrait.html">portrait</a>
+                                    <a href="nature.html">nature</a>
+                                    <a href="#">! street !</a>
+                                    <a href="#">! motion !</a>
+                                    <a href="#">! abstract !</a>
+                                    <a href="js-lightbox-test.html">lightbox js 3:2</a>
+                                    <a href="lightbox2:1.html">lightbox js 2:1</a>
+                                </div>
+                            </div> 
+                            <div class="dropdown">
+                                <button class="dropbtn select-able">products 
+                                    <!-- <i class="fa fa-caret-down"></i> -->
+                                </button>
+                                <div class="dropdown-content">
+                                    <a href="#">Link 1</a>
+                                    <a href="#">Link 2</a>
+                                    <a href="#">Link 3</a>
+                                    <a href="#">Link 4</a>
+                                </div>
+                            </div> 
+                            <a class="nav-link select-able" href="pages/contact.html">Contact</a>
+                        </nav>
+                        <div class="search-container">
+                            <form action="">
+                                <button type="submit" id="submit-btn" class="submit-btn hidden">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                                <input type="text" placeholder="Search enzee.com" name="search" id="search-input" class="hidden" required>
+                                <button type="" id="default-btn" class="default-btn">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                                <button type="" id="close-btn" class="close-btn hidden">X
+                                <!-- <i class="fa fa-times"></i> -->
+                                </button>
+                            </form>
+                        </div>
+                    </div>   */}
+
 // STICKY NAVBAR W3SCHOOLS
 
 // When the user scrolls the page, execute myFunction
