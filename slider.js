5c232f07418341e2166e847121729106de6d0697
gaborio1 <welcome2thepleasuredome@googlemail.com>
2021-06-19 23:04:26 +0100

    refactor slider.js to accept multiple slider divs in slider

--- slider.js
+++ slider.js
@@ -3,63 +3,44 @@
 // https://www.youtube.com/watch?v=ee8y1IV6pOI&t=1396s
 
 
-// ========== LANDSCAPE STYLE ==========
-const sliderLandscapeStyle = document.querySelector(".slider--landscape-style");
-const beforeLandscapeStyle = document.querySelector(".img-container-before--landscape-style");
-const afterLandscapeStyle = document.querySelector("img-container-after--landscape-style");
-const sliderContainerLandscapeStyle = document.querySelector(".slider-container--landscape-style");
+// THIS NOW WORKS WITH MULTIPLE SLIDER DIVS:
+const sliders = document.querySelectorAll(".slider");
+const befores = document.querySelectorAll(".before");
+// const afters = document.querySelectorAll(".after");
+const sliderDivs = document.getElementsByClassName("slider-div");
 
-const slideDivLandscapeStyle = (event) => {
-    // !!! CURSOR X POSITION WITHIN sliderContainerLandscapeStyle SEE COMMENT BELOW FOR layerX !!!
-    let xPos = event.layerX;
-    // The offsetWidth property returns the viewable width of an element in pixels, including padding, border and scrollbar, but not the margin.
-    let size = sliderContainerLandscapeStyle.offsetWidth;
-    // DYNAMICALLY RESIZE before WIDTH AS CURSOR MOVES WITHIN CONTAINER DIV
-    beforeLandscapeStyle.style.width = xPos + "px";
-    // ALSO, MOVE SLIDER DIV WITH CURSOR
-    sliderLandscapeStyle.style.left = xPos + "px";
-    // IF CURSOR IS NEAR LEFT/RIGHT EDGE, MAKE SLIDER AND before JUMP TO MIN AND MAX WIDTH
-    if (xPos < 10) {
-        beforeLandscapeStyle.style.width = 0;
-        // !!! TEMPORARY FIX FOR HIDING SLIDER !!!
-        sliderLandscapeStyle.style.left = "-2px";
-    }
-    if(xPos + 10 > size) {
-        beforeLandscapeStyle.style.width = size + "px";
-        sliderLandscapeStyle.style.left = size + 2 + "px"; 
-    }
-   
+for (let i = 0; i < sliderDivs.length; i++) {
+
+    sliderDivs[i].addEventListener("mousemove", function(event) {
+        // CURSOR X POSITION IN DIV BEING HOVERED
+        let xPos = event.layerX;
+        // WIDTH OF DIV BEING HOVERED
+        let size = sliderDivs[i].offsetWidth;
+        // ACCESS before AND slider IN CURRENT sliderDiv:
+        // DYNAMICALLY RESIZE before WIDTH AS CURSOR MOVES WITHIN CONTAINER DIV
+        befores[i].style.width = xPos + "px";
+        // ALSO, MOVE SLIDER DIV WITH CURSOR
+        sliders[i].style.left = xPos + "px";
+        // IF CURSOR IS NEAR LEFT/RIGHT EDGE, MAKE SLIDER AND before JUMP TO MIN AND MAX WIDTH
+        if (xPos < 10) {
+            befores[i].style.width = 0;
+            // !!! TEMPORARY FIX FOR HIDING SLIDER !!!
+            sliders[i].style.left = "-2px";
+        }
+        if(xPos + 10 > size) {
+            befores[i].style.width = size + "px";
+            // !!! TEMPORARY FIX FOR HIDING SLIDER !!!
+            sliders[i].style.left = size + 2 + "px";
+        }
+    }, false);
 }
+    
 
-sliderContainerLandscapeStyle.addEventListener("mousemove", slideDivLandscapeStyle);
-
-
-// ========== PORTRAIT STYLE ==========
-const sliderPortraitStyle = document.querySelector(".slider--portrait-style");
-const beforePortraitStyle = document.querySelector(".img-container-before--portrait-style");
-const afterPortraitStyle = document.querySelector("img-container-after--portrait-style");
-const sliderContainerPortraitStyle = document.querySelector(".slider-container--portrait-style");
-
-const slideDivPortraitStyle = (event) => {
-    let xPos = event.layerX;
-    let size = sliderContainerPortraitStyle.offsetWidth;
-    beforePortraitStyle.style.width = xPos + "px";
-    sliderPortraitStyle.style.left = xPos + "px";
-    if (xPos < 10) {
-        beforePortraitStyle.style.width = 0;
-        sliderPortraitStyle.style.left = "-2px";
-    }
-    if(xPos + 10 > size) {
-        beforePortraitStyle.style.width = size + "px";
-        sliderPortraitStyle.style.left = size + 2 + "px";
-    }
-}
-
-sliderContainerPortraitStyle.addEventListener("mousemove", slideDivPortraitStyle);
-
-
-// TAKEN FROM: https://developer.mozilla.org/en-US/docs/Web/API/UIEvent/layerX
-
+// *** layerX: ***
 // The UIEvent.layerX read-only property returns the horizontal coordinate of the event relative to the current layer.
 
 // This property takes scrolling of the page into account and returns a value relative to the whole of the document unless the event occurs inside a positioned element, where the returned value is relative to the top left of the positioned element.
+
+
+// *** offsetWidth: ***
+// The offsetWidth property returns the viewable width of an element in pixels, including padding, border and scrollbar, but not the margin.
