cbcd9e6e6729d7fa4761d08ea9a97b54875005fe
gaborio1 <welcome2thepleasuredome@googlemail.com>
2021-06-21 15:53:29 +0100

    add functions to slider div loop - MacBook

--- slider.js
+++ slider.js
@@ -2,39 +2,39 @@
 // "AWESOME VANILLA JAVASCRIPT IMAGE SLIDER" BY DEV ED
 // https://www.youtube.com/watch?v=ee8y1IV6pOI&t=1396s
 
+// THIS NOW CAN HANDLE MULTIPLE SLIDER DIVS:
 
-// THIS NOW WORKS WITH MULTIPLE SLIDER DIVS:
 const sliders = document.querySelectorAll(".slider");
 const befores = document.querySelectorAll(".before");
-// const afters = document.querySelectorAll(".after");
 const containers = document.getElementsByClassName("container");
+
+const changeWidth = (element, newWidth) => {
+    element.style.width = newWidth + "px";
+}
+
+const changeLeftPosition = (element, imgWidth = 0, pixels = 0) => {
+    element.style.left = imgWidth + pixels + "px";
+}
 
 for (let i = 0; i < containers.length; i++) {
 
-    containers[i].addEventListener("mousemove", function(event) {
-        // CURSOR X POSITION IN DIV BEING HOVERED
+    containers[i].addEventListener("mousemove", (event) => {
         let xPos = event.layerX;
-        // WIDTH OF DIV BEING HOVERED
-        let size = containers[i].offsetWidth;
-        // ACCESS before AND slider IN CURRENT sliderDiv:
-        // DYNAMICALLY RESIZE before WIDTH AS CURSOR MOVES WITHIN CONTAINER DIV
-        befores[i].style.width = xPos + "px";
-        // ALSO, MOVE SLIDER DIV WITH CURSOR
+        let divWidth = containers[i].offsetWidth;
+        changeWidth(befores[i], xPos);
+
         sliders[i].style.left = xPos + "px";
-        // IF CURSOR IS NEAR LEFT/RIGHT EDGE, MAKE SLIDER AND before JUMP TO MIN AND MAX WIDTH
         if (xPos < 10) {
-            befores[i].style.width = 0;
-            // !!! -2px TEMPORARY FIX FOR HIDING SLIDER !!!
-            sliders[i].style.left = "-2px";
+            changeWidth(befores[i], 0);
+            changeLeftPosition(sliders[i], divWidth, (-10-divWidth));
         }
-        if(xPos + 10 > size) {
-            befores[i].style.width = size + "px";
-            // !!! +2px TEMPORARY FIX FOR HIDING SLIDER !!!
-            sliders[i].style.left = size + 2 + "px";
+        if(xPos + 10 > divWidth) {
+            changeWidth(befores[i], divWidth);
+            changeLeftPosition(sliders[i], divWidth, 10);
         }
     }, false);
 }
-    
+
 
 // *** layerX: ***
 // The UIEvent.layerX read-only property returns the horizontal coordinate of the event relative to the current layer.
@@ -44,3 +44,46 @@ for (let i = 0; i < containers.length; i++) {
 
 // *** offsetWidth: ***
 // The offsetWidth property returns the viewable width of an element in pixels, including padding, border and scrollbar, but not the margin.
+
+// const sliders = document.querySelectorAll(".slider");
+// const befores = document.querySelectorAll(".before");
+// // const afters = document.querySelectorAll(".after");
+// const containers = document.getElementsByClassName("container");
+
+// const changeWidth = (element, newWidth) => {
+//     element.style.width = newWidth + "px";
+// }
+
+// const changeLeftPosition = (element, imgWidth = 0, pixels = 0) => {
+//     element.style.left = imgWidth + pixels + "px";
+// }
+
+// for (let i = 0; i < containers.length; i++) {
+
+//     containers[i].addEventListener("mousemove", (event) => {
+//         // CURSOR X POSITION IN DIV BEING HOVERED
+//         let xPos = event.layerX;
+//         // WIDTH OF DIV BEING HOVERED
+//         let divWidth = containers[i].offsetWidth;
+//         // ACCESS before AND slider IN CURRENT sliderDiv:
+//         // DYNAMICALLY RESIZE before WIDTH AS CURSOR MOVES WITHIN CONTAINER DIV
+//         changeWidth(befores[i], xPos);
+
+//         // ALSO, MOVE SLIDER DIV WITH CURSOR
+//         sliders[i].style.left = xPos + "px";
+//         // IF CURSOR IS NEAR LEFT/RIGHT EDGE, MAKE SLIDER AND before JUMP TO MIN AND MAX WIDTH
+//         if (xPos < 10) {
+//             changeWidth(befores[i], 0);
+//             // !!! -2px TEMPORARY FIX FOR HIDING SLIDER !!!
+//             // sliders[i].style.left = "-2px";
+//             changeLeftPosition(sliders[i], divWidth, (-10-divWidth));
+//         }
+//         if(xPos + 10 > divWidth) {
+//             // befores[i].style.width = divWidth + "px";
+//             changeWidth(befores[i], divWidth);
+//             // !!! +2px TEMPORARY FIX FOR HIDING SLIDER !!!
+//             // sliders[i].style.left = divWidth + 2 + "px";
+//             changeLeftPosition(sliders[i], divWidth, 10);
+//         }
+//     }, false);
+// }
