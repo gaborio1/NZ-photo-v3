fa8b0478a1928fc71c14c0301adcbfc4e6f41f26
gaborio1 <welcome2thepleasuredome@googlemail.com>
2021-07-25 23:30:49 +0100

    add .hover-pointer (home page) to common-rules.css, left div borders visible

--- src/scripts/footer.js
+++ src/scripts/footer.js
@@ -1,58 +1,70 @@
 // !!! ESCAPE SPECIAL CHARS WITH \ !!!
 // ( you're => you\'re )
 
-document.write('\
+document.write(
+  '\
 \
-    <div class="footer-top-img"><div class="footer-top-img-cover"><div class="footer-content--full-width full-width-1"><div class="footer-content-1"><div class="footer-info-container-1"><h2 class="brand-color">need a good photographer?</h2><h3>you\'re in the right place.</h3><a href="contact.html"><button class="footer-contact-button">contact us now</button></a></div></div></div><div class="footer-content--full-width full-width-2"><div class="footer-content-2"><div class="footer-2-text-container"></div><div class="footer-info-container-2"><div class="social-media-icon-container"><a href="#" class="social-media-icon fa fa-facebook"></a><a href="#" class="social-media-icon fa fa-twitter"></a><a href="#" class="social-media-icon fa fa-linkedin"></a><a href="#" class="social-media-icon fa fa-youtube"></a><a href="#" class="social-media-icon fa fa-instagram"></a></div></div></div></div></div></div><div class="footer-bottom"><p>Copyright © 2021, Enzee.com. All rights reserved.</p><div class="footer-links"><a href="#">Privacy Policy</a><a href="#">Terms of Use</a><a href="#">Language</a></div></div>\
+    <div class="footer-top-img"><div class="footer-top-img-cover"><div class="footer-content--full-width full-width-1"><div class="footer-content-1"><div class="footer-info-container-1"><h2 class="brand-color">need a good photographer?</h2><h3>you\'re in the right place.</h3><a href="contact.html"><button class="footer-contact-button hover-pointer">contact us now</button></a></div></div></div><div class="footer-content--full-width full-width-2"><div class="footer-content-2"><div class="footer-2-text-container"></div><div class="footer-info-container-2"><div class="social-media-icon-container"><a href="#" class="social-media-icon fa fa-facebook hover-pointer"></a><a href="#" class="social-media-icon fa fa-twitter hover-pointer"></a><a href="#" class="social-media-icon fa fa-linkedin hover-pointer"></a><a href="#" class="social-media-icon fa fa-youtube hover-pointer"></a><a href="#" class="social-media-icon fa fa-instagram hover-pointer"></a></div></div></div></div></div></div><div class="footer-bottom"><p>Copyright © 2021, Enzee.com. All rights reserved.</p><div class="footer-links"><a class="hover-pointer" href="#">Privacy Policy</a><a class="hover-pointer" href="#">Terms of Use</a><a class="hover-pointer" href="#">Language</a></div></div>\
 \
-');
-
+'
+);
 
 // ORIGINAL FOOTER HTML:
 
-{/* <div class="footer-top-img">
-                    <div class="footer-top-img-cover">
-
-                       <div class="footer-content--full-width full-width-1">
-                            <div class="footer-content-1">
-                                <div class="footer-info-container-1">
-                                    <h2 class="brand-color">need a good photographer?</h2>
-                                    <h3>you're in the right place.</h3>
-                                    <a href="contact.html">
-                                        <button class="footer-contact-button">
-                                            contact us now
-                                        </button>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="footer-content--full-width full-width-2">
-                            <div class="footer-content-2">
-                                <div class="footer-2-text-container">
-                                    <!-- <p>let our team help you find the best solution for you.</p> -->
-                                </div>
-                                    
-                                <div class="footer-info-container-2">
-                                    <div class="social-media-icon-container">
-                                        <a href="#" class="social-media-icon fa fa-facebook"></a>
-                                        <a href="#" class="social-media-icon fa fa-twitter"></a>
-                                        <a href="#" class="social-media-icon fa fa-linkedin"></a>
-                                        <a href="#" class="social-media-icon fa fa-youtube"></a>
-                                        <a href="#" class="social-media-icon fa fa-instagram"></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
+{
+  /* <div class="footer-top-img">
+          <div class="footer-top-img-cover">
+            <div class="footer-content--full-width full-width-1">
+              <div class="footer-content-1">
+                <div class="footer-info-container-1">
+                  <h2 class="brand-color">need a good photographer?</h2>
+                  <h3>you're in the right place.</h3>
+                  <a href="pages/contact.html">
+                    <button class="footer-contact-button hover-pointer">
+                      contact us now
+                    </button>
+                  </a>
                 </div>
-                    <!-- <img src="./images/footer-img-crop-1.jpg" alt="" class="footer-img"> -->
-                <div class="footer-bottom">
-                    <p>Copyright © 2021, Enzee.com. All rights reserved.</p>
-                    <div class="footer-links">
-                        <a href="#">Privacy Policy</a>
-                        <a href="#">Terms of Use</a>
-                        <a href="#">Language</a>
-                    </div>
-                </div> */}
+              </div>
+            </div>
+
+            <div class="footer-content--full-width full-width-2">
+              <div class="footer-content-2">
+                <div class="footer-2-text-container"></div>
+                <div class="footer-info-container-2">
+                  <div class="social-media-icon-container">
+                    <a
+                      href="#"
+                      class="social-media-icon fa fa-facebook hover-pointer"
+                    ></a>
+                    <a
+                      href="#"
+                      class="social-media-icon fa fa-twitter hover-pointer"
+                    ></a>
+                    <a
+                      href="#"
+                      class="social-media-icon fa fa-linkedin hover-pointer"
+                    ></a>
+                    <a
+                      href="#"
+                      class="social-media-icon fa fa-youtube hover-pointer"
+                    ></a>
+                    <a
+                      href="#"
+                      class="social-media-icon fa fa-instagram hover-pointer"
+                    ></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="footer-bottom">
+          <p>Copyright © 2021, Enzee.com. All rights reserved.</p>
+          <div class="footer-links">
+            <a class="hover-pointer" href="#">Privacy Policy</a>
+            <a class="hover-pointer" href="#">Terms of Use</a>
+            <a class="hover-pointer" href="#">Language</a>
+          </div>
+        </div> */
+}
