077ffc71774af19aece3260519380639ce214cc6
gaborio1 <welcome2thepleasuredome@googlemail.com>
2021-12-22 22:25:08 +0000

    add parallax layers to common footer (footer.js), no logic yet

--- src/scripts/navbar-content.js
+++ src/scripts/navbar-content.js
@@ -1,15 +1,15 @@
 if (window.innerWidth > 767) {
-  	console.log("navbar-content.js");
-  	// !!! ESCAPE SPECIAL CHARS WITH \ !!!
-  	// ( you're => you\'re )
+    console.log("navbar-content.js");
+    // !!! ESCAPE SPECIAL CHARS WITH \ !!!
+    // ( you're => you\'re )
 
-  	document.write(
-    	'\
+    document.write(
+        '\
 	\
 	<div class="header-content"><div class="logo-container"><div class="logo__block-row"><div class="logo__block logo__block-1"></div><div class="logo__block logo__block-2"></div><div class="logo__block logo__block-3"></div></div></div><nav><a id="navbar_home" class="page-link nav-link nav-link-text hover-pointer" href="../index.html">Home</a><a <a id="navbar__about" class="page-link nav-link nav-link-text hover-pointer" href="about.html">About</a><div class="nav-link--dropdown"><button id="navbar__portfolio" class="dropbtn nav-link-text hover-pointer">portfolio <!-- <i class="fa fa-caret-down"></i> --></button><div class="dropdown-content"><a class="page-link" href="model.html">model</a><a class="page-link" href="portrait.html">portrait</a><a class="page-link" href="nature.html">nature</a><a class="page-link" href="#">street</a><a class="page-link" href="#">motion</a><a class="page-link" href="#">abstract</a></div></div> <div class="nav-link--dropdown"><button id="navbar__products" class="dropbtn nav-link-text hover-pointer">products <!-- <i class="fa fa-caret-down"></i> --></button><div class="dropdown-content"><a class="page-link" href="products.html">Services</a><a class="page-link" href="before-after.html">Before/after</a></div></div> <a id="navbar__contact" class=" page-link nav-link nav-link-text hover-pointer" href="contact.html">Contact</a></nav><div class="search-container"><form action=""><button type="submit" id="submit-btn" class="submit-btn hidden"><i class="fa fa-search"></i></button><input type="text" autocomplete="off" placeholder="Search..." name="search" id="search-input" class="hidden" required><div  id="search-dropdown-content" class="search-dropdown-content hidden"><a href="about.html">About</a><a href="contact.html">Contact</a><a href="#privacypolicy">Privacy policy</a><a href="#reviews">Reviews</a><a href="#clients">Clients</a><a href="products.html">Products</a><a href="portrait.html">Portrait</a><a href="model.html">Model</a><a href="nature.html">Nature</a></div><button type="" id="default-btn" class="default-btn hover-pointer"><i class="fa fa-search"></i></button><button type="" id="close-btn" class="close-btn hidden">X<!-- <i class="fa fa-times"></i> --></button></form></div></div>\
 	\
 	'
-	);
+    );
 }
 
 {/* <div class="header-content">
