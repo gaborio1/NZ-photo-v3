613eed02274ee9c40a9fd83b9c562ece586ba7fa
gaborio1 <welcome2thepleasuredome@googlemail.com>
2021-12-26 11:54:33 +0000

    delete unused code in parallax.js

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -10,14 +10,10 @@ if (window.innerWidth > 768) {
 
 	// CONTROL NAVBAR OPACITY WITH SCROLL
 	let timelineNav = new TimelineMax()
-		// REVEAL NAVBAR ON SCROLL display: none -> flex
-		// .to("header", 1, { display: "flex" })
-		// .fromTo("header", 1, { opacity: 0 }, { opacity: 1 }, "-=1")
 		.fromTo("header", 1, { background: "rgba(0, 0, 0, 0.85)" }, { background: "rgba(0, 0, 0, 0.95)" }, "-=0");
 	new ScrollMagic.Scene({
 		triggerElement: ".image-container",
 		duration: "250%",
-		// duration: "100%",
 		triggerHook: 0,
 	})
 		.setTween(timelineNav)
@@ -31,28 +27,16 @@ if (window.innerWidth > 768) {
 	// MOUNTAIN LAYERS:
 
 	let timelineMountain = new TimelineMax()
-
-
 		.to("#mountain-crop-1", 3, { y: -800 }, "-=30")
-		// .to("#mountain-crop-1", 1, { y: -800 })
 		.to("#mountain-crop-2", 3, { y: -500 }, "-=30")
-		// .to("#mountain-crop-2", 1, { y: -500 }, "-=1")
 		.to("#mountain-crop-3", 3, { y: -250 }, "-=30")
-		// .to("#mountain-crop-3", 1, { y: -250 }, "-=1")
 		.to("#mountain-crop-4", 3, { y: -115 }, "-=30")
-		// .to("#mountain-crop-4", 1, { y: -115 }, "-=1")
 		.to("#mountain-crop-5", 3, { y: -30 }, "-=30")
-		// .to("#mountain-crop-5", 1, { y: -30 }, "-=1")
 		.to("#mountain-crop-6", 3, { y: -10 }, "-=30");
-	// .to("#mountain-crop-6", 1, { y: -10 }, "1=0");
-
-
-
 	// .to("#mountain-full", 3, { y: 100 }, "-=30");
 	new ScrollMagic.Scene({
 		triggerElement: ".image-container",
 		duration: "3000%",
-		// duration: "100%",
 		triggerHook: 0,
 	})
 		.setTween(timelineMountain)
@@ -66,38 +50,11 @@ if (window.innerWidth > 768) {
 	new ScrollMagic.Scene({
 		triggerElement: ".image-container",
 		duration: "3000%",
-		// duration: "100%",
 		triggerHook: 0,
 	})
 		.setTween(timelineContentAndTitle)
 		// .setPin(".image-container")
 		.addTo(controller);
-
-
-	// 					PINNING IMAGE CONTAINER:
-
-	// let timelineMountain = new TimelineMax()
-	// 	.to("#mountain-crop-1", 3, { y: -1000 }, "-=30")
-	// 	// .to("#mountain-crop-1", 3, { scale: 2, opacity: 0, ease: Linear.easeNone })
-	// 	.to("#mountain-crop-2", 3, { y: -600 }, "-=30")
-	// 	.to("#mountain-crop-3", 3, { y: -300 }, "-=30")
-	// 	.to("#mountain-crop-4", 3, { y: -60 }, "-=30")
-	// 	.to("#mountain-crop-5", 3, { y: 25 }, "-=30")
-	// 	.to("#mountain-full", 3, { y: 100 }, "-=30")
-	// 	.to("#title-div", 3, { top: "80%" }, "-=30")
-	// 	// MAIN CONTENT SLIDES ON TOP
-	// 	.to(".main-content", 3, { top: "100%" }, "-=30");
-	// new ScrollMagic.Scene({
-	// 	triggerElement: ".image-container",
-	// 	duration: "3000%",
-	// 	// duration: "100000px",
-	// 	triggerHook: 0,
-	// })
-	// 	.setTween(timelineMountain)
-	// 	// .setPin(".image-container")
-	// 	// THIS IS BETTER:
-	// 	.setPin(".parallax-layers-container")
-	// 	.addTo(controller);
 
 
 	// ========== ZOOMING IN ON MOUNTAIN LAYERS AT DIFFERENT SPEEDS ==========
@@ -112,7 +69,6 @@ if (window.innerWidth > 768) {
 	new ScrollMagic.Scene({
 		triggerElement: ".image-container",
 		duration: "3000%",
-		// duration: "100%",
 		triggerHook: 0,
 	})
 		.setTween(zoomSequence)
@@ -120,18 +76,6 @@ if (window.innerWidth > 768) {
 
 
 	let titleDivFadeOut = new TimelineMax()
-		// NOT IN USE:
-		// photography LETTERS FADEOUT (SEQUENCE: stagger)
-
-		// .fromTo(
-		// 	// ".photography__letter",
-		// 	// ".title-div-content",
-		// 	".enzee__letter",
-		// 	7,
-		// 	{ y: 0, filter: "blur(0px)", opacity: 1 },
-		// 	{ y: 0, filter: "blur(0px)", opacity: 0 },
-		// )
-
 		// Enzee
 		.fromTo(
 			".title-div__enzee-container",
@@ -345,17 +289,6 @@ if (window.innerWidth > 768) {
 	// ========== FOOTER PARALLAX: ==========
 
 	let textAndIconsFooter = new TimelineMax()
-		// SLOGAN AND CONTACT BUTTON FADEIN/SLIDE
-
-		// FOOTER INFO DIV WITH SLOGAN AND BUTTON TOGETHER:
-
-		// .fromTo(
-		// 	".footer-info-container-1",
-		// 	7,
-		// 	{ x: -300, y: -70, scale: 0.4, opacity: 0 },
-		// 	{ x: 0, y: 0, scale: 1, opacity: 1, ease: "easein", delay: 4, stagger: 0.8 },
-		// 	"-=0.5")
-
 		// SLOGAN SENTENCES/BUTTON IN SEQUENCE:
 		.fromTo(
 			".footer-info-content",
@@ -363,23 +296,14 @@ if (window.innerWidth > 768) {
 			{ x: -300, y: -70, scale: 0.5, filter: "blur(10px)", opacity: 0 },
 			{ x: 0, y: 0, scale: 1, filter: "blur(0px)", opacity: 1, ease: "easein", delay: 4, stagger: 10 },
 			"-=0.5")
-
-		// CLEAR DIV:
-		// .fromTo(
-		// 	".footer-info-container-2",
-		// 	7,
-		// 	{ x: -300, scale: 0.5, filter: "blur(10px)", opacity: 0 },
-		// 	{ x: 0, scale: 1, filter: "blur(0px)", opacity: 1, ease: "easein", delay: 4, stagger: 10 },
-		// 	"-=0")
-
-		// SOCIAL MEDIA ICONS FADEIN/SLIDE (SEQUENCE: stagger)
+		// CLEAR DIV
 		.fromTo(
 			".full-width-2",
 			7,
 			{ y: 100, scale: 1, filter: "blur(10px)", opacity: 0 },
 			{ y: 0, scale: 1, filter: "blur(0px)", opacity: 1, ease: "easein" },
 			"-=0")
-
+		// SOCIAL MEDIA ICONS FADEIN/SLIDE (SEQUENCE: stagger)
 		.fromTo(".social-media-icon",
 			5,
 			{ x: 200, opacity: 0 },
@@ -423,20 +347,5 @@ if (window.innerWidth > 768) {
 	})
 		.setTween(zoomCameraFooter)
 		.addTo(controller);
-
-	// NOT IN USE AS ICONS SLIDE IN ONE BY ONE !!!
-	// SOCIAL MEDIA ICON CONTAINER (ALL ICONS SLIDE WITH CONTAINER)
-
-	// let timelineSocialMediaContainer = new TimelineMax()
-	// 	.to(".social-media-icon-container", 10, { x: "-200" }, "-=10")
-	// new ScrollMagic.Scene({
-	// 	triggerElement: ".footer-content-2",
-	// 	duration: "100%",
-	// 	triggerHook: 0.5,
-	// 	// offset: 10
-	// })
-	// 	.setTween(timelineSocialMediaContainer)
-	// 	// .setPin(".image-container")
-	// 	.addTo(controller);
 
 }
